475782f4f5791a5d320b76fcffae5990cd9b9bfd
Agner Bezerra <agnerbzrao@hotmail.com>
2023-04-24 21:13:38 -0300

    first commit, it was added the react project with typescript, vite, eslint and prettier

---
+++ react-project/src/components/Home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+    return <div>
+            <h1>Aqui estou!!!</h1>
+    </div>
+}
+
+export default Home;
---
+++ react-project/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './components/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/upload',
+    element: <Home />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
